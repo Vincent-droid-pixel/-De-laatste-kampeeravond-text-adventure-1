2b9d36081859a08f69b615f0e82b4c178fd4362c
Vinciento <>
2021-06-08 11:51:09 +0000

    helpmenu toegevoegd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 #kamers
 kamers = {
   "RivierMetBrug" : {
@@ -8,17 +7,13 @@ kamers = {
     "beschrijving" : "Het is een gammele houten brug die naar het noorden over de rivier loopt. Je weet niet of je er eigenlijk wel overheen kunt lopen zonder dat hij doorbreekt. Je ziet ook nog een pad langs het water stroomopwaarts richting het oosten lopen.",
     "opties" : ["n", "o"]
   },
-
 }
-
 #de gameloop
 def game(gebied):
   locatie = kamers[gebied]
-
   #titel en beschrijving van kamer verkrijgen#
   titel = locatie["titel"]
   beschrijving = locatie["beschrijving"]
-
   #kamer beschrijven
   os.system('clear')
   print("=============================================")
@@ -29,7 +24,6 @@ def game(gebied):
 
 #naam invullen#
 def NaamInvullen():
-  os.system('clear')
   print("=============================================")
   print("|                                           |")
   print("|                                           |")
@@ -41,33 +35,51 @@ def NaamInvullen():
   print("=============================================")
   speler_naam = input()
   speler.naam = speler_naam
-
   format_string = "Hallo %s, leuk dat je deze text adventure speelt!"
   print(format_string % speler.naam)
 
-  choice = input(""" Schrijf OK als je verder wil gaan: """)
-  if choice == "OK":
+  choice = input(""" Typ een willekeurige letter om verder te gaan: """)
+  if choice == "":
     game("RivierMetBrug")
-
-
+  else:
+    game("RivierMetBrug")
+  
 class speler:
   def  spelernaam(zelf):
     zelf.naam = ''
-
 speler = speler()
-
 
 #intro#
 def intro():
-  os.system('clear')
   print("======================================================")
   print("Je bent gezellig met vrienden aan het kamperen in het bos. Helaas kon je niet slapen. Je dacht: 'laat ik even een boswandeling maken, zodat ik beter kan slapen.' Het enige wat je meeneemt is een kompas.")
   print("")
   print("======================================================")
   choice = input(""" Schrijf OK als je verder wil gaan: """)
   if choice == "OK":
-     #zorgen dat de speler naam kan invullen
+    os.system('clear')
+    #zorgt dat de speler naam kan invullen
     NaamInvullen()
+  else:
+    os.system('clear')
+    print("=============================================")
+    print("|       vul een geldig antwoord in!!!       |")
+    intro()
+
+#helpmenu
+def help():
+  print("=============================================")
+  print("Je hebt verschillende controls in deze text adventure. Zo kun je met ‘get item [g]’ en ‘drop item [d]’ items oppakken en laten vallen / neerleggen. Verder kan je met ‘inventory [i]’ alle items zien die je hebt opgepakt. Ook kan je nog met de gegeven windrichtingen (Noord [n], Oost [o], Zuid [z], West [w]) bij elk gebied, naar het volgende gebied gaan. (Ten slotte kan je met ‘health [h]’ je health zien.) Al deze controls kan je tijdens het gehele spel gebruiken.")
+  print("=============================================")
+  choice = input(""" Typ 'Menu' als je terug naar het menu wil gaan: """)
+  if choice == "Menu":
+    os.system('clear')
+    hoofdmenu()
+  else:
+    os.system('clear')
+    print("=============================================")
+    print("|       vul een geldig antwoord in!!!       |")
+    help()
 
 
 #hoofdmenu#
@@ -88,13 +100,16 @@ def hoofdmenu():
            Vul je keuze in:  """)   
   #keuzes van het hoofdmenu#
   if choice == "Start":
+    os.system('clear')
     intro()
   elif choice == "start":
+    os.system('clear')
     intro()
   elif choice == "Stop":
     sys.exit
-  elif choice == "Stop":
-    print("help")
+  elif choice == "help":
+    os.system('clear')
+    help()
   else:
     os.system('clear')
     print("=============================================")
