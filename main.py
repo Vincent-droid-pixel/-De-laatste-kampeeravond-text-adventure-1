3dd569fa003deea861de4bd8527b5931cddb6458
Vinciento <>
2021-06-08 11:33:28 +0000

    Hoofdmenu gemaakt

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+#hoofdmenu#
+print("=============================================")
+print("|                                           |")
+print("|                                           |")
+print("|     ----------------------------------    |")
+print("|     || Welkom bij de text adventure ||    |")
+print("|     ||    van Sannah en Vincent     ||    |")
+print("|     ----------------------------------    |")
+print("|                 Kies uit:                 |")
+print("|                                           |")
+print("|                 ~Start~                   |")
+print("|                 ~Stop~                    |")
+print("|                 ~Help~                    |")
+print("|                                           |")
+print("|                                           |")
+print("=============================================") 
