9031e90a59b37ee6a5ff4368cc796785d08b23cd
Vinciento <>
2021-06-08 11:34:37 +0000

    Hoofdmenu is werkend

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import os
+import sys
 
-#hoofdmenu#
-print("=============================================")
-print("|                                           |")
-print("|                                           |")
-print("|     ----------------------------------    |")
-print("|     || Welkom bij de text adventure ||    |")
-print("|     ||    van Sannah en Vincent     ||    |")
-print("|     ----------------------------------    |")
-print("|                 Kies uit:                 |")
-print("|                                           |")
-print("|                 ~Start~                   |")
-print("|                 ~Stop~                    |")
-print("|                 ~Help~                    |")
-print("|                                           |")
-print("|                                           |")
-print("=============================================") 
+def hoofdmenu():
+  print("=============================================")
+  print("|                                           |")
+  print("|                                           |")
+  print("|     ----------------------------------    |")
+  print("|     || Welkom bij de text adventure ||    |")
+  print("|     ||    van Sannah en Vincent     ||    |")
+  print("|     ----------------------------------    |")
+  print("|                 Kies uit:                 |")
+  choice = input("""|                                           |
+|                  Start                    |
+|                  Stop                     |
+|                  Help                     |  
+=============================================               
+           Vul je keuze in:  """)   
+  if choice == "Start":
+    print("Start")
+  elif choice == "Stop":
+    sys.exit
+  elif choice == "Stop":
+    print("help")
+  else:
+    os.system('clear')
+    print("=============================================")
+    print("|       vul een geldig antwoord in!!!       |")
+    hoofdmenu()
+
+
+hoofdmenu()
