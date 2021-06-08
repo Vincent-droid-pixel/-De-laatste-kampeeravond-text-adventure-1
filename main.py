219357a560cce4c858253e76d6b982d40877d786
sannah24 <>
2021-06-08 11:47:32 +0000

    possibility om je naam in te vullen. start aan gameloop.

--- main.py
+++ main.py
@@ -1,12 +1,61 @@
 import os
 import sys
 
-#kamerbeschrijving#
-def kamerbeschrijving():
+#kamers
+kamers = {
+  "RivierMetBrug" : {
+    "titel" : "een rivier met een gammele brug.",
+    "beschrijving" : "Het is een gammele houten brug die naar het noorden over de rivier loopt. Je weet niet of je er eigenlijk wel overheen kunt lopen zonder dat hij doorbreekt. Je ziet ook nog een pad langs het water stroomopwaarts richting het oosten lopen.",
+    "opties" : ["n", "o"]
+  },
+
+}
+
+#de gameloop
+def game(gebied):
+  locatie = kamers[gebied]
+
+  #titel en beschrijving van kamer verkrijgen#
+  titel = locatie["titel"]
+  beschrijving = locatie["beschrijving"]
+
+  #kamer beschrijven
   os.system('clear')
   print("=============================================")
-
+  print(f"Je komt aan bij {titel}")
+  print(beschrijving)
   print("=============================================")
+  print("typ de windrichting waar je heen wil gaan: ")
+
+#naam invullen#
+def NaamInvullen():
+  os.system('clear')
+  print("=============================================")
+  print("|                                           |")
+  print("|                                           |")
+  print("|                                           |")
+  print("|              wat is je naam?              |")
+  print("|                                           |")
+  print("|                                           |")
+  print("|                                           |")
+  print("=============================================")
+  speler_naam = input()
+  speler.naam = speler_naam
+
+  format_string = "Hallo %s, leuk dat je deze text adventure speelt!"
+  print(format_string % speler.naam)
+
+  choice = input(""" Schrijf OK als je verder wil gaan: """)
+  if choice == "OK":
+    game("RivierMetBrug")
+
+
+class speler:
+  def  spelernaam(zelf):
+    zelf.naam = ''
+
+speler = speler()
+
 
 #intro#
 def intro():
@@ -17,7 +66,9 @@ def intro():
   print("======================================================")
   choice = input(""" Schrijf OK als je verder wil gaan: """)
   if choice == "OK":
-    kamerbeschrijving()
+     #zorgen dat de speler naam kan invullen
+    NaamInvullen()
+
 
 #hoofdmenu#
 def hoofdmenu():
@@ -37,7 +88,7 @@ def hoofdmenu():
            Vul je keuze in:  """)   
   #keuzes van het hoofdmenu#
   if choice == "Start":
-        intro()
+    intro()
   elif choice == "start":
     intro()
   elif choice == "Stop":
@@ -50,5 +101,4 @@ def hoofdmenu():
     print("|       vul een geldig antwoord in!!!       |")
     hoofdmenu()
                                         
-
 hoofdmenu()
